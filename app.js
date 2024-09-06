3ce7b16dda4dc4692e17ee2d1559bfe43b232bd7
anaeng <36653124+anaeng@users.noreply.github.com>
2024-09-05 22:06:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -26,28 +26,12 @@ function pesquisar() {
         }
     }
 
-    // Se não houver resultados para o termo pesquisado, exibe uma mensagem
+    // Se não houver resultados, exibe uma mensagem
     if (resultados === "") {
         resultados = `<p class="mensagem-erro">Nenhum resultado encontrado para "${pesquisa}".</p>`;
     }
 
     // Atribui os resultados gerados à seção HTML
     section.innerHTML = resultados;
-
-    // Remove a classe 'oculto' para mostrar a seção de resultados
-    section.classList.remove('oculto');
 }
-
-// Adiciona o evento de clique no botão
-document.addEventListener('DOMContentLoaded', function() {
-    const botaoPesquisar = document.querySelector('button');
-
-    // Inicialmente oculta a caixa de resultados
-    const section = document.getElementById("resultados-pesquisa");
-    section.classList.add('oculto');
-
-    // Adiciona o evento de clique ao botão de pesquisa
-    botaoPesquisar.addEventListener('click', pesquisar);
-});
-
 
